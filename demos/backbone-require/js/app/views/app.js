bfbefbf0666e49b9b899d9646039f1fde553d067
Lars Johnson <larsj@mit.edu>
2014-03-24 13:19:28 -0500

    Backbone demo using require.js

---
+++ demos/backbone-require/js/app/views/app.js
@@ -0,0 +1,75 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'collections/picset',
+    'views/pic',
+    'text!templates/stats.html',
+    'common'
+], function($, _, Backbone, PicSet, PicView, statsTemplate, Common) {
+    /**
+     * The top-level piece of UI for the App.
+     */
+    var AppView = Backbone.View.extend({
+        el: "#picpikapp",
+        
+        statsTemplate: _.template(statsTemplate),
+        
+        events: {
+            "click #add-pic": "createPic"
+        },
+        
+        initialize: function() {            
+            this.listenTo(PicSet, 'add', this.addOne);
+            this.listenTo(PicSet, 'reset', this.addAll);
+            this.listenTo(PicSet, 'all', this.render);
+            this.listenTo(PicSet, 'change:completed', this.filterOne);
+            this.listenTo(PicSet, 'filter', this.filterAll);
+            
+            this.$main = $('#main');
+            
+            this.$footer = $('#footer-stats');
+            
+            PicSet.fetch();
+        },
+        
+        render: function() {
+            var deleted = PicSet.deleted().length;
+            var favorited = PicSet.favorited().length;
+                        
+            this.$footer.html(this.statsTemplate({
+                num_favorited: favorited,
+                num_deleted: deleted
+            }));
+            
+            this.$('#filters a').removeClass('selected')
+            .filter('[href="#/' + (Common.picFilter || '') + '"]')
+            .addClass('selected');
+            
+        },
+        
+        addOne: function(pic) {
+            var view = new PicView({model: pic});
+            this.$('#pic-grid').append(view.render().el);
+        },
+        
+        addAll: function() {
+            PicSet.each(this.addOne, this);
+        },
+        
+        filterOne: function (pic) {
+            pic.trigger('visible');
+        },
+        
+        filterAll: function() {
+            PicSet.each(this.filterOne, this);
+        },
+        
+        createPic: function(e) {
+            console.log('create pic!');
+            PicSet.create({title: "New Pic"});
+        },
+        
+    });
+    return AppView;
+});
